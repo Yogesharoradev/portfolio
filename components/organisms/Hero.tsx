7e26b95a086377cbd0607a411e3eb1a4bb0437d5
Hemant <hrsmart2150@gmail.com>
2023-08-04 15:40:42 +0530

    hero section done completely

--- components/organisms/Hero.tsx
+++ components/organisms/Hero.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Button from '@/components/atoms/Button'
 import HeroImage from '@/components/atoms/HeroImage'
 import Socials from '@/components/molecules/Socials'
 
@@ -9,18 +10,20 @@ const Hero: React.FC = () => {
             <section id="home" className="wrapper scroll-mt-[20rem] block sm:flex justify-between items-center mb-[2.75rem] md:mb-[5.75rem] mt-[4rem] lg:mt-[7rem]">
                 <div>
                     <h1 className='before:content-["///"] before:h-[300px] before:text-[175px] before:font-[700] before:-z-10 before:select-none before:translate-x-[-95%] before:translate-y-0 webkit_text_stroke before:opacity-[0.25] before:tracking-[-.1em] before:absolute
-                     text-[96px] text-white font-semibold'>
+                    text-[65px] md:text-[96px] text-white font-semibold'>
                         Hemant
                     </h1>
-                    <h4 className='mt-4 mb-[1.7rem]'>
+                    <h4 className='mt-4 text-[22px] md:text-[26px]'>
                         Hobbyist illustrator and designer.
                         <br className='' />
                         Attempting to program.
                     </h4>
-                    <div className="">
+                    <div className="mt-4 mb-[1.7rem]">
                         <Socials />
                     </div>
-                    {/* <Button side on: click={handleClick} on: keypress={handleClick}>Discover my work ↓</Button> */}
+                    <Button>
+                        Discover my work ↓
+                    </Button>
                 </div>
                 <HeroImage />
             </section >
