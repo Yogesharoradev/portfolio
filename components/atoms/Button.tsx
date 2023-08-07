73e41ad6ac5133b47799faaaadd44bcda592d3ac
Hemant <hrsmart2150@gmail.com>
2023-08-07 20:26:28 +0530

    commented err code

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import useSound from "use-sound";
+// import useSound from "use-sound";
 // import clicksound from "../../public/sounds/click.mp3";
 
 interface ButtonProps {
@@ -7,13 +7,13 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ children }) => {
-    const [play] = useSound("../../public/sounds/click.mp3", {
-        volume: 1
-    });
+    // const [play] = useSound("../../public/sounds/click.mp3", {
+    //     volume: 1
+    // });
 
     return (
         <button
-            onClick={play}
+            // onClick={play}
             style={{ transition: 'filter 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94), transform 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94)' }}
             className='rounded-[16px] flex items-center justify-center relative w-full sm:w-max cursor-pointer bg-neutral_one select-none mt-5 py-4 px-8 md:py-4 md:pr-8 md:pl-12 text-yellow font-[300] text-[18px] md:text-[1.6rem] font-jetbrains tracking-[-.075em] border border-neutral_four active:scale-[95%] hover:brightness-[110%] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-yellow'
         >
