11267572567e7859d7ba306ce8ec65cfa0134ebe
Hemant <hrsmart2150@gmail.com>
2024-06-05 10:08:44 +0530

    Added resume

--- components/atoms/ResumeButton.tsx
+++ components/atoms/ResumeButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const ResumeButton: React.FC = () => {
   return (
     <Link
-      href="https://dread.s3.ap-south-1.amazonaws.com/Hemant_CV.pdf"
+      href="/resume.pdf"
       target="_blank"
       className="select-none fixed top-2 right-2 m-4 flex items-center justify-center w-12 h-12 rounded-full bg-accent_opacity text-accent bg-opacity-50 animate-[spin_15s_linear_infinite] hover:scale-125 transition-all z-20 offset_ring"
     >
