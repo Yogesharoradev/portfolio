c5b92a25342aa5acee144721896d048b4a695d35
YogeshArora <Yogesharora207@gmail.com>
2025-09-18 14:18:15 +0530

    changes the mobile number

--- components/molecules/Socials.tsx
+++ components/molecules/Socials.tsx
@@ -51,7 +51,7 @@ const Socials: React.FC = () => {
           ></path>
         </svg>
       </Social>
-      <Social tip="Call Me" link="tel:+918427992959">
+      <Social tip="Call Me" link="tel:+918437579399">
         <svg
           width="24px"
           height="24px"
